32a4483242e4552f1b4094c3ff0bf01db5b003d6
weijiqiang <weijq@cychina.cn>
2019-06-05 21:03:53 +0800

    安装antd vue
    Signed-off-by: weijiqiang <weijq@cychina.cn>

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+/* 
+ * @Title: webpack手动配置信息
+ * @Description: Todo 
+ * @Author: weijq@cychina.cn (韦继强) 
+ * @Date: 2019-06-05 20:19:14 
+ * @Last Modified by: weijq@cychina.cn (韦继强)
+ * @Last Modified time: 2019-06-05 20:19:38
+ * @Version:V1.0 
+ * Company: 合肥安慧软件有限公司 
+ * Copyright: Copyright (c) 2017' 
+ */
+
+module.exports = {
+    devServer:{
+        open:true,//配置自动打开浏览器
+        hotOnly:true,//热加载
+    }
+}

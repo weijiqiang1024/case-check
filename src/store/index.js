65b8a61acedcbf4a7c794bf10d13ca33b76bbf0d
weijq <772981712@qq.com>
2019-06-07 22:55:00 +0800

    store/index.js
    Signed-off-by: weijq <772981712@qq.com>

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+/* 
+ * @Title: $undefined 
+ * @Description: Todo 
+ * @Author: weijq@cychina.cn (韦继强) 
+ * @Date: 2019-06-07 22:53:45 
+ * @Last Modified by: weijq@cychina.cn (韦继强)
+ * @Last Modified time: 2019-06-07 22:54:33
+ * @Version:V1.0 
+ * Company: 合肥安慧软件有限公司 
+ * Copyright: Copyright (c) 2017' 
+ */
+
+
+export default {
+    
+}

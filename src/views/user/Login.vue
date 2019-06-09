4963bb2f38141fad3118950b1de83eea9e5c0970
weijq <772981712@qq.com>
2019-06-09 23:03:12 +0800

    添加路由信息 Signed-off-by: weijq <772981712@qq.com>

---
+++ src/views/user/Login.vue
@@ -0,0 +1,27 @@
+/* 
+ * @Title: $user login 
+ * @Description: Todo 
+ * @Author: weijq@cychina.cn (韦继强) 
+ * @Date: 2019-06-09 21:08:10 
+ * @Last Modified by: weijq@cychina.cn (韦继强)
+ * @Last Modified time: 2019-06-09 22:18:45
+ * @Version:V1.0 
+ * Copyright: Copyright (c) 2017' 
+ */
+
+ <template>
+     <div>login page</div>
+ </template>
+ 
+ <script>
+ export default {
+     data(){
+         return {}
+     }
+ }
+ </script>
+ 
+ <style>
+ 
+ </style>
+ 
---
+++ src/views/user/Users.vue
@@ -0,0 +1,26 @@
+/* 
+ * @Title: $Users 
+ * @Description: Todo 
+ * @Author: weijq@cychina.cn (韦继强) 
+ * @Date: 2019-06-09 22:43:02 
+ * @Last Modified by: weijq@cychina.cn (韦继强) 
+ * @Last Modified time: 2019-06-09 22:43:02 
+ * @Version:V1.0 
+ * Copyright: Copyright (c) 2017' 
+ */
+
+<template>
+    <div>用户管理</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {}
+    }
+}
+</script>
+
+<style>
+
+</style>
